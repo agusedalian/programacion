1b893a0e24234417d01c416ad5a7c3d8443033db
agusedalian <medalian@itba.edu.ar>
2023-11-10 11:30:33 -0300

    entrega

--- contacto.js
+++ contacto.js
@@ -1,12 +1,12 @@
-const comentarios = document.getElementById("Cont");
+const comentarios = document.getElementById("contactarnos");
 
 comentarios.addEventListener("click", function() {
-  confirmar("Contactarnos");
+  confirmar("enviar");
 });
 
 function confirmar(com) {
-  const confirm = confirm("¿Deseas confirmar " + com + "?");
-    if (confirm){
+  const confirmacion = confirm("Hola! ¿Deseas " + com + " tu contacto?");
+    if (confirmacion){
         window.location.href = "contact.html";
     } else{
         alert("Se eligió no confirmar. Podrás seguir investigando sobre viajes y si deseas contactarte podras hacerlo!")
